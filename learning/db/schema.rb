1ff7c47f9ed7f824e8fe757eb2b483cf75afe036
Peter <enter0621@gmail.com>
2021-10-12 23:51:33 +0800

    1. 實作 middleware => 將 param 傳入的 token 比對後，取出 current_user 寫入 grape 的環境變數 2. helper / authenticate => 根據傳入的環境變數取得 current_user，達成判斷權限的功能 3. 例外處理 4. 新增 api_access_token 表，紀錄 user token

--- learning/db/schema.rb
+++ learning/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211012090812) do
+ActiveRecord::Schema.define(version: 20211012095319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_access_tokens", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "account"
